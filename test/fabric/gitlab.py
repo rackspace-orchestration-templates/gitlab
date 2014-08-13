bd351e5e530f7ccb25ad5239bb3ff8d9c36155eb
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-08-13 08:40:44 -0500

    initializing tests

---
+++ test/fabric/gitlab.py
@@ -0,0 +1,26 @@
+from fabric.api import env, run, task
+from envassert import detect, file, group, package, port, process, service, \
+    user
+
+
+@task
+def check():
+    env.platform_family = detect.detect()
+
+    assert package.installed("nginx")
+    assert package.installed("mysql-server-5.5")
+    assert file.exists("/srv/git/gitlab/vendor/bundle/ruby/1.9.1/bin/unicorn_rails")
+    assert file.exists("/srv/git/bin/ruby")
+    assert port.is_listening(80)
+    assert port.is_listening(443)
+    assert port.is_listening(3306)
+    assert port.is_listening(6379)
+    assert user.exists("git")
+    assert group.is_exists("git")
+    assert user.is_belonging_group("git", "git")
+    assert process.is_up("nginx")
+    assert process.is_up("mysqld")
+    assert process.is_up("redis-server")
+    assert service.is_enabled("nginx")
+    assert service.is_enabled("mysqld")
+    assert service.is_enabled("gitlab")
