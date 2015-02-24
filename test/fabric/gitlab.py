2ed3294ed28818303d562950dad1543a284ed8ec
Jason Boyles <jason@duncancreek.net>
2015-02-24 16:24:44 -0600

    add test assert strings

--- test/fabric/gitlab.py
+++ test/fabric/gitlab.py
@@ -8,24 +8,24 @@ from hot.utils.test import get_artifacts
 def check():
     env.platform_family = detect.detect()
 
-    assert package.installed("nginx")
-    assert package.installed("mysql-server-5.5")
+    assert package.installed("nginx"), "package nginx is not installed"
+    assert package.installed("mysql-server-5.5"), "package mysql-server-5.5 is not installed"
     assert file.exists(
-        "/srv/git/gitlab/vendor/bundle/ruby/1.9.1/bin/unicorn_rails")
-    assert file.exists("/srv/git/bin/ruby")
-    assert port.is_listening(80)
-    assert port.is_listening(443)
-    assert port.is_listening(3306)
-    assert port.is_listening(6379)
-    assert user.exists("git")
-    assert group.is_exists("git")
-    assert user.is_belonging_group("git", "git")
-    assert process.is_up("nginx")
-    assert process.is_up("mysqld")
-    assert process.is_up("redis-server")
-    assert service.is_enabled("nginx")
-    assert service.is_enabled("mysqld")
-    assert service.is_enabled("gitlab")
+        "/srv/git/gitlab/vendor/bundle/ruby/1.9.1/bin/unicorn_rails"), "unicorn is not configured"
+    assert file.exists("/srv/git/bin/ruby"), "ruby is not installed"
+    assert port.is_listening(80), "port 80 is not listening"
+    assert port.is_listening(443), "port 443 is not listening"
+    assert port.is_listening(3306), "port 3306 is not listening"
+    assert port.is_listening(6379), "port 6379 is not listening"
+    assert user.exists("git"), "there is no git user"
+    assert group.is_exists("git"), "there is no git group"
+    assert user.is_belonging_group("git", "git"), "user git is not in the git group"
+    assert process.is_up("nginx"), "nginx is not running"
+    assert process.is_up("mysqld"), "mysqld is not running"
+    assert process.is_up("redis-server"), "redis-server is not running"
+    assert service.is_enabled("nginx"), "service nginx is not enabled"
+    assert service.is_enabled("mysqld"), "service mysqld is not enabled"
+    assert service.is_enabled("gitlab"), "service gitlab is not enabled"
 
 
 @task
